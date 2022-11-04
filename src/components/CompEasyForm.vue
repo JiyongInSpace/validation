9bbbf7d6c1285fc5d5ecc137eb6058ef8c3a62dc
JiyongInSpace <blaziken@naver.com>
2022-11-04 21:45:04 +0900

    .

--- src/components/CompEasyForm.vue
+++ src/components/CompEasyForm.vue
@@ -1,27 +1,58 @@
 <template>
-    <div>
-        <Form
-            name="easyForm"
-            id="easyForm"
-            class="d-flex flex-column"
-        >
+    <Form
+        name="easyForm"
+        id="easyForm"
+    >
+        <div class="field_container">
             <Field
-                name="InputId"
+                name="IdField"
+                v-slot="{ field }"
+                :rules="IdRules"
             >
+                <div class="input-container">
+                    <div class="input-wrapper">
+                        <span>
+                            ID
+                        </span>
+                        <input 
+                            class="InputId"
+                            v-bind="field"
+                        />
+                    </div>
+    
+                    <ErrorMessage name="IdField"></ErrorMessage>
+                </div>
             </Field>
 
             <Field
-                name="InputPassword"
-                type="password"
+                name="PasswordField"
+                v-slot="{ field }"
+                :rules="PasswordRules"
             >
+                <div class="input-container">
+                    <div class="input-wrapper">
+                        <span>
+                            Password
+                        </span>
+                        <input 
+                            class="InputPassword"
+                            v-bind="field"
+                        />
+                    </div>
+    
+                    <ErrorMessage name="PasswordField"></ErrorMessage>
+                </div>
             </Field>
-        </Form>
-    </div>   
+
+        </div>
+    </Form>
 </template>
 
 <script lang="ts">
     import { defineComponent } from 'vue';
-    import { Form, Field } from "vee-validate";
+    import { Form, Field, ErrorMessage } from "vee-validate";
+    import * as Yup from "yup"
+
 
     export default defineComponent({
         name: 'CompEasyForm',
@@ -29,11 +60,38 @@
         components: {
             Form, 
             Field,
+            ErrorMessage,
         },
 
+        setup() {
+            const IdRules = (_value: string) => {
+                if(!_value){
+                    return "NO VALUE";
+                }
+            }
+            
+            const PasswordRules = (_value: string) => {
+                if(!_value){
+                    return "NO PASSWORD";
+                }
+            }
+
+            // schema 적용
+            // const schema = Yup.object({
+            //     id: Yup.string().required(),
+            //     password: Yup.string(),
+            //     email: Yup.string().email(),
+            //     age: Yup.number().required().positive().integer(),
+            // })
+
+            return {
+                IdRules,
+                PasswordRules,
+                // schema,
+            }
+        },
     });
 </script>
 
-<style>
-
+<style lang="scss">
 </style>
